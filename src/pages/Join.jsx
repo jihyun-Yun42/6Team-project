b0fb7ab0344bf7327ebb578dbe48b9f82fe3fbab
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-03-24 16:11:22 +0900

    [add] message: 푸터 구역 생성

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import FooterDelivery from '../components/FooterDelivery'
 import HeaderDelivery from '../components/HeaderDelivery'
 
 function Join() {
   return (
     <>
     <HeaderDelivery/>
-      Join
+    <StyledDiv></StyledDiv>
+    <FooterDelivery/>
     </>
   )
 }
 
 export default Join
+
+const StyledDiv =styled.div`
+  height: 1000px;
+  background-color: #b5aeae;
+`
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import FooterDelivery from '../components/FooterDelivery'
 import HeaderDelivery from '../components/HeaderDelivery'
 
 function Login() {
   return (
     <>
     <HeaderDelivery/>
-    Login
+    <StyledDiv></StyledDiv>
+    <FooterDelivery/>
     </>
   )
 }
 
 export default Login
+
+const StyledDiv =styled.div`
+  height: 1000px;
+  background-color: #b5aeae;
+`
