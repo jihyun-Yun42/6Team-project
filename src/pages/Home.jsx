f0cbee82e79cb93371cbf44e6db786a40a418780
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-03-25 11:04:33 +0900

    [tempSave] 회원가입 페이지 제작중

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Banner from '../components/Banner';
-import Nav from '../components/Nav';
+import React from "react";
+import Banner from "../components/Banner";
+import Nav from "../components/Nav";
 
 function Home() {
   return (
     <>
       <Nav />
-      <div style={{ margin: '0 auto', maxWidth: '1200px' }}>
+      <div style={{ margin: "0 auto", maxWidth: "1200px" }}>
         <Banner />
       </div>
     </>
