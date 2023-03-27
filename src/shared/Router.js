364eb588d9c8654e3a143c6295c8551d43212b11
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-03-27 11:09:01 +0900

    [feat] 일반 회원가입 페이지 초안 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,22 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import GlobalStyle from '../components/GlobalStyle'
-import DeliveryHome from '../pages/DeliveryHome'
-import Home from '../pages/Home'
-import Join from '../pages/Join'
-import Login from '../pages/Login'
-import StoreSearch from '../pages/StoreSearch';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "../components/GlobalStyle";
+import DeliveryHome from "../pages/DeliveryHome";
+import Home from "../pages/Home";
+import Join from "../pages/Join";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import StoreSearch from "../pages/StoreSearch";
 
 function Router() {
   return (
     <BrowserRouter>
-      <GlobalStyle/>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/deliveryHome" element={<DeliveryHome />} />
         <Route path="/storeSearch" element={<StoreSearch />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
